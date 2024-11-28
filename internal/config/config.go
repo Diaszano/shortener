7f8ce69a8a6dfcfac75ade86ea1b813213ce50fa
diaszano <61257292+Diaszano@users.noreply.github.com>
2024-11-27 23:23:15 -0300

    feat(app): Add environment variables
    Add reading and validation of project environment variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DatabaseDriver string `mapstructure:"DATABASE_DRIVER"`
+	DatabaseHost   string `mapstructure:"DATABASE_HOST"`
+	DatabasePort   uint   `mapstructure:"DATABASE_PORT"`
+	DatabaseUser   string `mapstructure:"DATABASE_USER"`
+	DatabasePass   string `mapstructure:"DATABASE_PASS"`
+	DatabaseName   string `mapstructure:"DATABASE_NAME"`
+}
+
+var cfg *Config
+
+func LoadConfig(path string) *Config {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		panic(err)
+	}
+
+	return cfg
+}
