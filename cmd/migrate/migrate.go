f3de4213e5aa0890e86d808829fcc256682fedc4
diaszano <61257292+Diaszano@users.noreply.github.com>
2024-12-04 20:51:46 -0300

    feat: updated import of logger and add error in logger

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -5,9 +5,11 @@ package main
 import (
 	"database/sql"
 	"errors"
-	"log"
+	"fmt"
 	"os"
 	"strings"
+
+	"go.uber.org/zap"
 
 	"github.com/Diaszano/shortener/internal/config"
 	"github.com/golang-migrate/migrate/v4"
@@ -19,43 +21,47 @@ import (
 // main is the entry point of the migration tool. It initializes the database connection,
 // sets up the migration instance, and executes migration commands based on the provided arguments.
 func main() {
+
+	logger := config.GetLogger()
+	defer config.CloseLogger()
+
 	db, err := sql.Open("postgres", config.Env.Database.Dsn())
 	if err != nil {
-		log.Fatalf("Failed to connect to the database: %v", err)
+		logger.Fatal("Failed to connect to the database", zap.Error(err))
 	}
 
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
-		log.Fatalf("Failed to initialize database driver: %v", err)
+		logger.Fatal("Failed to initialize database driver", zap.Error(err))
 	}
 
 	m, err := migrate.NewWithDatabaseInstance("file:internal/database/migrations", "postgres", driver)
 	if err != nil {
-		log.Fatalf("Failed to initialize migrations: %v", err)
+		logger.Fatal("Failed to initialize migrations", zap.Error(err))
 	}
 
 	switch cmd := os.Args[len(os.Args)-1]; strings.ToLower(cmd) {
 	case "up":
 		if err := m.Up(); err != nil {
 			if errors.Is(err, migrate.ErrNoChange) {
-				log.Println("No migrations to apply (up-to-date).")
+				logger.Info("No migrations to apply (up-to-date).")
 			} else {
-				log.Fatalf("Failed to apply migrations: %v", err)
+				logger.Fatal("Failed to apply migrations", zap.Error(err))
 			}
 		} else {
-			log.Println("Migrations applied successfully.")
+			logger.Info("Migrations applied successfully.")
 		}
 	case "down":
 		if err := m.Down(); err != nil {
 			if errors.Is(err, migrate.ErrNoChange) {
-				log.Println("No migrations to revert (already clean).")
+				logger.Info("No migrations to revert (already clean).")
 			} else {
-				log.Fatalf("Failed to revert migrations: %v", err)
+				logger.Fatal("Failed to revert migrations", zap.Error(err))
 			}
 		} else {
-			log.Println("Migrations reverted successfully.")
+			logger.Info("Migrations reverted successfully.")
 		}
 	default:
-		log.Fatalf("Invalid option: %s", cmd)
+		logger.Fatal(fmt.Sprintf("Invalid option: %s", cmd))
 	}
 }
